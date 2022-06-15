b18e2715ff2922d55e7284fa96fe7e6a4197cced
Aaron Pennington <aaronpennington@Aarons-Mac-mini.lan>
2022-06-15 14:34:32 -0600

    Adding a storage stack

--- stacks/index.js
+++ stacks/index.js
@@ -1,16 +1,12 @@
-import { MyStack } from "./MyStack";
-import { App } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
 
-/**
- * @param {App} app
- */
-export default function (app) {
+export default function main(app) {
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
-    srcPath: "api",
+    srcPath: "backend",
     bundle: {
       format: "esm",
     },
   });
-  app.stack(MyStack);
+  app.stack(StorageStack);
 }
