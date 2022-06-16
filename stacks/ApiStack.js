5288fb2f0669d8689bb66a78bc0d79e2570d7a43
Aaron Pennington <aaronpennington@Aarons-Mac-mini.lan>
2022-06-15 18:02:19 -0600

    Enabling CORS

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -16,6 +16,7 @@ export function ApiStack({ stack, app }) {
         },
       },
     },
+    cors: true,
     routes: {
       "POST /notes": "functions/create.main",
       "GET /notes/{id}": "functions/get.main",
